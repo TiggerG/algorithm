6b976b6d3b6dc3fdced07f3f739be77ae55c6f46
liubaosong <liubaosong1998@163.com>
2020-09-19 10:18:41 +0800

    t919

--- src/T200909.java
+++ src/T200909.java
@@ -1,5 +1,9 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author ：liubaosong
  * @date ：Created in 2020/9/9 19:06
@@ -75,4 +79,110 @@ public class T200909 {
         String[] strings=new String[]{"length","leng","lensss"};
         System.out.println(t2(strings));
     }
+
+    /**
+     * 买股票问题
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+
+            int i = 0;
+            int valley = prices[0];
+            int peak = prices[0];
+            int maxprofit = 0;
+            while (i < prices.length - 1) {
+                while (i < prices.length - 1 && prices[i] >= prices[i + 1])
+                    i++;
+                valley = prices[i];
+                while (i < prices.length - 1 && prices[i] <= prices[i + 1])
+                    i++;
+                peak = prices[i];
+                maxprofit += peak - valley;
+            }
+            return maxprofit;
+
+    }
+
+
+    @Test
+    public void test3(){
+        int[] arr = new int[]{7,1,5,3,6,4};
+        System.out.println(maxProfit(arr));
+    }
+
+    /**
+     * 旋转数组
+     */
+    public static int[] rotate(int[] nums, int k) {
+        if (k>nums.length){
+            k = k%nums.length;
+        }
+        int l = nums.length;
+        int[] num2 = new int[nums.length];
+        for (int i=0 ;i<k;i++){
+            num2[i]=nums[l-k+i];
+        }
+        for (int j=0;j<l-k;j++){
+            num2[k+j]=nums[j];
+        }
+        nums=num2;
+        return nums;
+    }
+    @Test
+    public void test4(){
+        int[] arr = new int[]{1,2,3,4,5,6,7};
+        int[] b=rotate(arr,3);
+       for (int i=0;i<b.length;i++){
+           System.out.println(b[i]);
+       }
+    }
+
+    /**
+     * 三数之和
+     */
+
+
+        public List<List<Integer>> threeSum(int[] nums) {
+            int n = nums.length;
+            Arrays.sort(nums);
+            List<List<Integer>> list = new ArrayList<>();
+            for (int first = 0;first<n;++first){
+                //要合上一个数字不同
+                if (first>0&&nums[first]==nums[first-1]){
+                    continue;
+                }
+                //c指针指向数组的最后一个元素
+                int third = n-1;
+                //目标元素
+                int target = -nums[first];
+                for (int second = first+1;second<n;++second){
+                    if (second>first+1&&nums[second]==nums[second-1]){
+                        continue;
+                    }
+                    // 需要保证 b 的指针在 c 的指针的左侧
+                    while (second < third && nums[second] + nums[third] > target) {
+                        --third;
+                    }
+                    if (second == third){
+                        break;
+                    }
+
+                    if (nums[second]+nums[third]==target){
+                        List<Integer> list1 = new ArrayList<>();
+                        list1.add(first);
+                        list1.add(second);
+                        list1.add(third);
+                        list.add(list1);
+                    }
+
+
+                }
+            }
+            return list;
+        }
+
+
+
+
 }
